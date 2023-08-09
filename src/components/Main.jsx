c87d4048685081d0110e00b90376041ef1650055
samresh9 <thapasamresh9@gmail.com>
2023-08-10 00:14:40 +0545

    updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,17 @@
 import { useEffect } from "react";
 function Main ({children , showSavedImg , savedImg , onShowModal, setSavedImg}){
+    useEffect(() => {
+      const savedImgs = JSON.parse(localStorage.getItem("savedImgs"));
+      if (savedImgs) {
+        setSavedImg(savedImgs);
+      }
+    }, [setSavedImg]);
+
+    useEffect(() => {
+      localStorage.setItem("savedImgs", JSON.stringify(savedImg));
+    }, [savedImg]);
+
+
     return (
       <>
         <div className="justify-center ">
@@ -8,30 +20,18 @@ function Main ({children , showSavedImg , savedImg , onShowModal, setSavedImg}){
       </>
     );
 }
-function SavedImageList({ savedImg , onShowModal , setSavedImg }) {
-
-  useEffect(() => {
-    const savedImgs = JSON.parse(localStorage.getItem("savedImgs"));
-    if (savedImgs) {
-      setSavedImg(savedImgs);
-    }
-  }, [setSavedImg]);
-
-  useEffect(() => {
-    localStorage.setItem("savedImgs", JSON.stringify(savedImg));
-  }, [savedImg]);
+function SavedImageList({ savedImg , onShowModal  }) {
 
 
   return (
-    <>    
-    <h1 className="text-center text-blue-500 ">Saved Images</h1>
-    <div className="grid gap-4 px-4 mx-auto mt-10 mb-5 grid-container md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl">
-      {savedImg?.map((image, i) => (
-        <SavedImage key={i} data={image} onShowModal={onShowModal} />
-      ))}
-    </div>
+    <>
+      {savedImg && <h1 className="mt-3 text-4xl font-bold text-center text-blue-500">Your Saved Images</h1>}
+      <div className="grid gap-4 px-4 mx-auto mt-5 mb-5 grid-container md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl">
+        {savedImg?.map((image, i) => (
+          <SavedImage key={i} data={image} onShowModal={onShowModal} />
+        ))}
+      </div>
     </>
-
   );
 }
 function SavedImage({data , onShowModal}){
